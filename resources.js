e265063ee1de3688f5750ede5b60895365fec6c6
Olusola Obajuluwa <olusolaobajuluwa@gmail.com>
2026-03-13 00:31:28 +0100

    created the resource repository

---
+++ resources.js
@@ -0,0 +1,51 @@
+const resources = [
+  {
+    title: "Goat Farming Technical",
+    type: "pdf",
+    category: "Animal production",
+    subcategory: "goat",
+    file: "books/Goat Farming Technical.pdf"
+  },
+  {
+    title: "Indigenous_goat_handbook",
+    type: "pdf",
+    category: "Animal production",
+    subcategory: "goat",
+    file: "books/indigenous_goat_handbook.pdf"
+  },
+  {
+    title: "Pig production training manual Original",
+    type: "pdf",
+    category: "Animal production",
+    subcategory: "pig",
+    file: "books/pig production training manual Original"
+  },
+ {
+    title: "PIGGERY-MANUAL-SENT-RF",
+    type: "pdf",
+    category: "Animal production",
+    subcategory: "pig",
+    file: "books/PIGGERY-MANUAL-SENT-RF"
+  },
+  {
+    title: "Sheep management",
+    type: "pdf",
+    category: "Animal production",
+    subcategory: "sheep",
+    file: "books/Sheep management"
+  },
+  {
+    title: "Nutrition and management of sheep and goat",
+    type: "link",
+    category: "Animal production",
+    subcategory: "goat",
+    url: "https://www.fao.org/4/ah221e/AH221E05.htm"
+  },
+  {
+    title: "Small scale poultry production",
+    type: "link",
+    category: "Animal production",
+    subcategory: "poultry",
+    url: "https://www.fao.org/4/y5169e/y5169e00.htm"
+  }
+];
