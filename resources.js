a0a1379e2011624bc690bb26c68c153df2d41269
Olusola Obajuluwa <olusolaobajuluwa@gmail.com>
2026-03-13 16:12:06 +0100

    Added list of books by Mr. Martins

--- resources.js
+++ resources.js
@@ -18,21 +18,21 @@ const resources = [
     type: "pdf",
     category: "Animal production",
     subcategory: "pig",
-    file: "books/pig production training manual Original"
+    file: "books/pig production training manual Original.pdf"
   },
  {
     title: "PIGGERY-MANUAL-SENT-RF",
     type: "pdf",
     category: "Animal production",
     subcategory: "pig",
-    file: "books/PIGGERY-MANUAL-SENT-RF"
+    file: "books/PIGGERY-MANUAL-SENT-RF.pdf"
   },
   {
     title: "Sheep management",
     type: "pdf",
     category: "Animal production",
     subcategory: "sheep",
-    file: "books/Sheep management"
+    file: "books/Sheep management.pdf"
   },
   {
     title: "Nutrition and management of sheep and goat",
@@ -47,5 +47,36 @@ const resources = [
     category: "Animal production",
     subcategory: "poultry",
     url: "https://www.fao.org/4/y5169e/y5169e00.htm"
-  }
+  },
+
+  // Mr. Martins
+  {
+    title: "Fundamentals-of-Agronomy",
+    type: "pdf",
+    category: "Crop production",
+    subcategory: "Agronomy",
+    file: "books/Mr_martins/AgriMoon-AAG-111-Fundamentals-of-Agronomy.pdf"
+  },
+  {
+    title: "Agronomy-book",
+    type: "pdf",
+    category: "Crop production",
+    subcategory: "Agronomy",
+    file: "books/Mr_martins/agronomy-book.pdf"
+  },
+  {
+    title: "Principles of agronomy agricultural meteorology",
+    type: "pdf",
+    category: "Crop production",
+    subcategory: "Agronomy",
+    file: "books/Mr_martins/principles-of-agronomy-agricultural-meteorology-signed.pdf"
+  },
+  {
+    title: "The_principles_of_agronomy",
+    type: "pdf",
+    category: "Crop production",
+    subcategory: "Agronomy",
+    file: "books/Mr_martins/The_principles_of_agronomy_-_a_text-book_of_crop_production_for_high-schools_and_short-courses_in_agricultural_colleges.pdf"
+  },
+  
 ];
