23716f2a4e8d5deb5486ab65d7a0121c5bb8196a
Olusola Obajuluwa <olusolaobajuluwa@gmail.com>
2026-03-17 08:24:18 +0100

    Added the course list, page, and style

---
+++ courses.js
@@ -0,0 +1,12 @@
+courses =[
+    {
+        title: "Principles of Animal Production",
+        code: "AGT 114",
+        url: "books/Mr_martins/agronomy-book.pdf",
+    },
+    {
+        title: "Sheep, goat and swine production",
+        code: "AGT 123",
+        url: "books/Sheep management.pdf"
+    }
+]
