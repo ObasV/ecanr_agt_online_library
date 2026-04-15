827e83227408ba9aa68491b9a9831ffa695810ac
Olusola Obajuluwa <olusolaobajuluwa@gmail.com>
2026-04-15 15:36:08 +0100

    include the directoy for the added course materials

--- courses.js
+++ courses.js
@@ -1,12 +1,17 @@
 courses =[
     {
-        title: "Principles of Animal Production",
+        title: "AGT 114 - Principles of Animal Production SECOND PART",
         code: "AGT 114",
-        url: "books/Mr_martins/agronomy-book.pdf",
+        url: "course_materials/AGT 114 - Principles of Animal Production SECOND PART.pdf",
     },
     {
-        title: "Sheep, goat and swine production",
-        code: "AGT 123",
-        url: "books/Sheep management.pdf"
+        title: "AGT 126 MICRO LIVESTOCK PRODUCTION NOTES",
+        code: "AGT 126",
+        url: "course_materials/AGT 126 MICRO LIVESTOCK PRODUCTION NOTES.pdf"
+    },
+    {
+        title: "AGT 211 MICRO LIVESTOCK PRODUCTION NOTES",
+        code: "AGT 211",
+        url: "course_materials/AGT 211_PASTURE AND RANGE MANAGEMENT_Improvement and Management Practices of Pastures and Forage Crops.docx"
     }
 ]
